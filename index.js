f5124a640b34b0b157159630b6b25c3db62394fe
Sander <sander@wieni.be>
2020-05-12 08:39:56 +0200

    Initial commit 🎼

---
+++ index.js
@@ -0,0 +1,25 @@
+module.exports = {
+  parser: "babel-eslint",
+  extends: ["airbnb", "prettier", "prettier/react"],
+  plugins: ["react", "prettier"],
+  env: {
+    browser: true,
+  },
+  rules: {
+    "react/jsx-filename-extension": [1, { extensions: [".js", ".jsx"] }],
+    "import/prefer-default-export": "off",
+    "prettier/prettier": "error",
+    "import/no-unresolved": "off",
+  },
+  settings: {
+    "import/resolver": {
+      node: {
+        extensions: [".js", ".jsx", ".ts", ".tsx"],
+        moduleDirectory: ["node_modules", "resources"],
+      },
+    },
+  },
+  globals: {
+    Drupal: "writable",
+  },
+};
